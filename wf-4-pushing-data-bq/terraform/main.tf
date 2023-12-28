491175d264be66bc00d227a5c6489f808d5b1161
mukesh sain <mukeshk.sain786@gmail.com>
2023-12-28 10:07:35 +0100

    ch

--- wf-4-pushing-data-bq/terraform/main.tf
+++ wf-4-pushing-data-bq/terraform/main.tf
@@ -1,19 +1,18 @@
 provider "google" {
-  project = "plated-hash-405319"
-  region = "us-east1"
+    project = "plated-hash-405319"
+    region = "us-east1"
 }
 
-#resource which stores the TF state file in the bucket
 terraform {
   backend "gcs" {
-    bucket = "tf_statebucket_4_side_pocs_bq_with_schema"
-    prefix = "value"
+    bucket = "tf_statebucket_4_side_pocs"
+    value = "bushing_data_bq"
   }
 }
 
 #Bigquery dataset where CF will publish the data
-resource "google_bigquery_dataset" "bq_dataset_4_wf_4_bq_with_schema" {
-    dataset_id = "bq_dataset_4_wf_4_bq_with_schema_id"
+resource "google_bigquery_dataset" "bq_dataset_4_wf_4_pushing_data_bq" {
+    dataset_id = "bq_dataset_4_wf_4_pushing_data_bq"
     project = "plated-hash-405319"
     location = "us-east1"
     default_table_expiration_ms = 3600000
@@ -24,10 +23,10 @@ resource "google_bigquery_dataset" "bq_dataset_4_wf_4_bq_with_schema" {
 }
 
 #Bigquery table where CF will publish the data
-resource "google_bigquery_table" "bq_table_4_wf_4_bq_with_schema" {
-  dataset_id = google_bigquery_dataset.bq_dataset_4_wf_4_bq_with_schema.dataset_id
-  table_id = "bq_table_4_wf_4_bq_with_schema"
-  schema = jsonencode(jsondecode(file("schema/bq_table1_schema.json")))
+resource "google_bigquery_table" "bq_table_4_wf_4_pushing_data_bq" {
+  dataset_id = google_bigquery_dataset.bq_dataset_4_wf_4_pushing_data_bq.dataset_id
+  table_id = "bq_table_4_wf_4_pushing_data_bq"
+  schema = jsonencode(jsondecode(file("schema/bq_table1_schema.jsonl")))
 
 #Below example shows if you want to hardcode the schema in the file itself
 #  schema = <<EOF
